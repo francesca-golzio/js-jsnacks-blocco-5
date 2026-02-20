810d5c4a03f6f0abbb3ac73c66c30f1558f3d122
Francesca Golzio <francescagolzio@gmail.com>
2026-02-20 17:41:00 +0100

    complete snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,18 @@
+console.log('🌿');
+
+
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// 👇 forEach() con funzione anonima
+/* names.forEach(function(element) {
+  console.log(element)
+}
+); */
+
+// 👇 forEach() con arrow function
+names.forEach((element) =>
+  console.log(element)
+);
