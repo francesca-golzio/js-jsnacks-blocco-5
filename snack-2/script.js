19500bac43ecab86b9e5457e888e0590ac4912da
Francesca Golzio <francescagolzio@gmail.com>
2026-02-20 18:46:45 +0100

    complete snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,3 +1,5 @@
+console.log('🌿');
+
 const people = [
   { name: 'Paolo', age: 35 },
   { name: 'Giulia', age: 24 },
@@ -6,3 +8,26 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+/* 👇 con il ciclo for() */
+/* for (let index = 0; index < people.length; index++) {
+  const person = people[index].name;
+  console.log(person);  
+} */
+
+/* 👇 con forEach() */
+/* people.forEach(function(person) {
+  console.log(person.name);  
+}); */
+
+/* 👇 con forEach() e arrow function */
+/* people.forEach(person => console.log(person.name)); */
+
+
+/* 👇 con map() */
+/* let names = people.map(function(person){
+  console.log(person.name);  
+}) */
+
+/* 👇 con map() e arrow function */
+let names = people.map(person => console.log(person.name));
