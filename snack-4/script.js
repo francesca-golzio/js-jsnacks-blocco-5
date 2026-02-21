3ba4dbb9a157c1c0e79adf67e4b20505316b8af9
Francesca Golzio <francescagolzio@gmail.com>
2026-02-21 17:21:39 +0100

    complete snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,3 +1,6 @@
+console.log('🌿');
+
+
 const posts = [
   { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
@@ -7,3 +10,40 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+/* 👇 con for() e .push() */
+/* const authors = [];
+for(i = 0; i < posts.length; i++) {
+  let authorName = posts[i].author;
+  console.log(authorName);
+  authors.push(authorName);
+  //console.log(authors);  
+}
+console.log(authors);  */
+
+/* 👇 con .forEach() e .push() */
+/* const authors = [];
+posts.forEach(function(post) {
+  let authorName = post.author;
+  //console.log(authorName);
+  authors.push(authorName);
+  //console.log(authors);  
+})
+console.log(authors);   */
+
+/* 👇 con .forEach(), arrow function e .push() */
+/* const authors = [];
+posts.forEach(post => authors.push(post.author))
+console.log(authors);   */
+
+/* 👇 con .map() */
+/* const authors = posts.map(function (post) {
+  //console.log(post.author);
+  return post.author;  
+})
+console.log(authors); */
+
+/* 👇 con .map() e arrow function */
+const authors = posts.map(post => post.author);
+console.log(authors);
