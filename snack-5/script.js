2d234ef64d36c8f2085193b51c0e98904782c9f9
Francesca Golzio <francescagolzio@gmail.com>
2026-02-21 17:59:20 +0100

    complete snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,28 @@
+console.log('🌿');
+
+
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+/* 👇 con .forEach() e .push() */
+/* let evenNums = [];
+nums.forEach(function(num){
+  if(num % 2 === 0) {
+    evenNums.push(num);
+  }
+  return evenNums;
+})
+console.log(evenNums); */
+
+/* 👇 con .filter() */
+/* let evenNums = nums.filter(getEvenNums);
+function getEvenNums(num) {
+  return num % 2 === 0;
+}
+console.log(evenNums); */
+
+/* 👇 con .filter() versione breve con arrow function*/
+let evenNums = nums.filter(num => (num % 2 === 0));
+console.log(evenNums);
